dcc24750f0d62a2644aae9e2d6afc48f53db74f6
evily kauane <20231ireinfint0019@ifba.edu.br>
2026-05-21 08:37:04 -0300

    apaguei a classe de redes pyton

---
+++ Redes.java
@@ -0,0 +1,9 @@
+public class Redes {
+
+    public static void main(String[] args){
+        System.out.println(" fis uma alteração");
+
+
+    }
+
+}
--- Redes.py
+++
@@ -1,3 +0,0 @@
-def funcoes():
-    print("Trabalho")
-funcoes();
